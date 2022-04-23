d11c915edcfa1f5a284668e2fd2cb9b77f026709
paquisha <paquishaxd@gmail.com>
2022-04-22 21:12:53 -0500

    se agrega ejercicio de prueba de inicio

---
+++ anteriores/programa2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+int main(void){
+	char varA = 10;
+	int varB = 150;
+	float varC = 1.25 ;
+
+	printf("el valor de la variable A es: %d \n", varA);
+	printf("el valor de la variable B es: %d \n", varB);
+	printf("el valor de la variableC es: %.3f \n", varC);
+
+	return 0;
+}
